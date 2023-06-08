de6609cea399fedec7f45f5bc590b4d7a2288e32
Everson Carvalho <eversoncarvalho@Eversons-MacBook-Air.local>
2023-06-08 18:12:21 -0300

    trying to remove navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Hero from '@/components/hero/Index';
 import Main from '@/components/main/Index';
-import Navbar from '@/components/navbar/Index';
 import Portoflio from '@/components/portfolio/Index';
 import About from '@/components/about/Index';
 import ContactForm from '@/components/contactForm/Index';
@@ -15,7 +14,6 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Navbar />
       <Hero />
       <Main />
       <Portoflio />
